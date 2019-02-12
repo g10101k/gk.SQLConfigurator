7965e71faf965df3c8f07ada6e35c97aebf38762
Artem <47314881+komodias@users.noreply.github.com>
2019-02-12 11:18:03 +0300

    Update SQLItems.xml
    Add new positions of SQL-query Characteristics OU and Users

--- gk.SQLConfigurator/Settings.cs
+++ gk.SQLConfigurator/Settings.cs
@@ -1,4 +1,21 @@
-﻿namespace gk.SQLConfigurator.Properties {
+/*
+ *  "gk.SQLConfigurator", Excel add-in that allows you to fill / edit / delete SQL table data.
+ *
+ *  Copyright (C) 2015-2019  Igor Tyulyakov aka g10101k, g101k. Contacts: <g101k@mail.ru>
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+namespace gk.SQLConfigurator.Properties {
     
     
     // This class allows you to handle specific events on the settings class:
